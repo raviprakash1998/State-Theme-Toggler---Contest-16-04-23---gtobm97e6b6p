b716c9ae540cbce3f8d51deb98c4c0898d76b2d2
Ravi Prakash <120682913+raviprakash1998@users.noreply.github.com>
2023-04-16 11:50:27 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -2,8 +2,19 @@ import React, { useState } from "react";
 import "../styles/App.css";
 
 export default function App() {
+    const [theme,setTheme] = useState("light")
+    const func =()=>{
+                if(theme==="light"){
+
+                    setTheme("dark")
+                }else{
+                    setTheme("light")
+                }
+
+            }
+
     return (
-        <div className="App">
+        <div className="App" id={theme==="light" ? "light" :"dark"}>
             <h1>Newton School</h1>
             <div>
                 <form>
